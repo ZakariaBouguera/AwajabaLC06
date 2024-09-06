4c1981415fbc07822b84c712248537eda70b6b65
Zak <Zakaria.bouguera.sio@gmail.com>
2024-09-06 15:54:14 +0200

    Classe repas

---
+++ repas.java
@@ -0,0 +1,11 @@
+public class Repas{
+	private int numero ; 
+	private LocalDate date;
+
+	public Repas (int numero, LocalDate date){
+	this.numero = numero;
+	this.LocalDate = LocalDate;
+	}
+
+
+}
